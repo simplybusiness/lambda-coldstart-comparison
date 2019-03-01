bf0e41fa98550880eb3543b0df7e0ef0ece2f021
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2019-03-01 13:52:30 +0000

    Add utility functions to list API Gateways and corresponding resources

--- download-header.js
+++ download-header.js
@@ -3,6 +3,7 @@
 const AWS = require('aws-sdk');
 AWS.config.region = 'eu-west-1';
 const Lambda = new AWS.Lambda();
+const APIGateway = new AWS.APIGateway();
 
 const START_TIME = new Date('2019-02-27T09:00:00.000Z');
 
@@ -24,7 +25,45 @@ const listFunctions = async function (marker, acc) {
   }
 };
 
+const listGateways = async function (position, acc) {
+  acc = acc || [];
+
+  const resp = await APIGateway.getRestApis({ limit: 100, position }).promise();
+
+  const gateways = resp.items
+    .map(i => ({ id: i.id, name: i.name }))
+    .filter(item => item.name.includes("aws-coldstart"))
+
+  acc = acc.concat(gateways);
+
+  if (resp.position) {
+    return await listGateways(resp.position, acc);
+  } else {
+    return acc;
+  }
+};
+
+const listResources = async function (restApiId, position, acc) {
+  acc = acc || [];
+
+  const resp = await APIGateway.getResources({ restApiId, limit: 100, position }).promise();
+
+  const resources = resp.items
+    .filter(item => item.pathPart)
+    .map(item => ({id: item.id, path: item.path, name: item.pathPart}));
+
+  acc = acc.concat(resources);
+
+  if (resp.position) {
+    return await listResources(resp.position, acc);
+  } else {
+    return acc;
+  }
+}
+
 module.exports = {
   START_TIME: START_TIME,
   listFunctions: listFunctions,
+  listGateways: listGateways,
+  listResources: listResources,
 }
